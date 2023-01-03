d5539e770e89f1ed442f58c1ac043b2a2aec824c
은지 <100760303+soondong2@users.noreply.github.com>
2023-01-03 15:39:16 +0900

    Create retail_sales.sql

---
+++ 3장 시계열 분석/retail_sales.sql
@@ -0,0 +1,15 @@
+-- 테이블을 생성합니다.
+DROP table if exists retail_sales;
+CREATE table retail_sales
+(
+sales_month date
+,naics_code varchar
+,kind_of_business varchar
+,reason_for_null varchar
+,sales decimal
+)
+;
+
+-- CSV 파일에서 데이터를 읽어 테이블에 채워넣습니다.
+-- 본인이 다운로드 한 CSV 파일의 경로로 수정하세요
+COPY retail_sales FROM '파일 경로\Dataus_retail_sales.csv' DELIMITER ',' CSV HEADER;
